03a8b85131d383ba4dd5acfb74be67e839ed2dc7
wild.chamo <joselitobedoya230@gmail.com>
2021-05-16 18:44:22 -0500

    var let const

---
+++ src/scope/global.js
@@ -0,0 +1,15 @@
+// asignada de forma global 
+var hello= "hello!";
+let world = "world";
+const helloWorld= 'Hello world';
+// console.log(hello)
+
+
+const anotherFunction = ()=>{
+    console.log(hello)
+    console.log(world)
+    console.log(helloWorld)
+}
+
+anotherFunction();
+//scope global
