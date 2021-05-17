ce6a092199574cc5cac634f3d2ff4a3291703248
wild.chamo <joselitobedoya230@gmail.com>
2021-05-16 22:44:15 -0500

    block scope

---
+++ src/scope/block.js
@@ -0,0 +1,51 @@
+const fruits = ()=>{
+    if (true){
+        var fruits1='apple';
+    }
+    let fruits2='banana';
+    const fruits3='kiwi'
+    console.log(fruits1);
+    console.log(fruits2);
+    console.log(fruits3);
+}
+
+fruits();
+
+const fruits = ()=>{
+    if (true){
+        var fruits1='apple';
+        let fruits2='banana';
+        const fruits3='kiwi'
+    }
+    console.log(fruits1);
+    console.log(fruits2);
+    console.log(fruits3);
+}
+
+fruits();
+//con let y const no podes llamarlo desde otro lugar que no sea el bloque donde lo declaraste!
+
+let x=1;
+{
+    let x=2;
+    console.log(x);
+}
+
+console.log(x);
+
+var x=1;
+{
+    var x=2;
+    console.log(x);
+}
+
+console.log(x);
+
+
+const anotherFunction=()=>{
+    for(var i=0;i<10;i++){
+        setTimeout(() => {
+            console.log(i);
+        }, 1000);
+    }
+}
--- src/scope/function.js
+++ src/scope/function.js
@@ -14,3 +14,4 @@ const otraFuncion =()=>{
     console.log(x,y)
 }
 otraFuncion();
+
