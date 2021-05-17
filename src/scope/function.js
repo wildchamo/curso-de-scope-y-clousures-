d4a656b426269ebadcef0d9fd58e49f3e73eca33
wild.chamo <joselitobedoya230@gmail.com>
2021-05-16 22:33:45 -0500

    function scope

---
+++ src/scope/function.js
@@ -0,0 +1,16 @@
+const fruits = ()=>{
+    var fruit='apple';
+    console.log(fruit);
+}
+fruits();
+console.log(fruit);
+
+
+const otraFuncion =()=>{
+    var x=1;
+    var x=2;
+    let y=1;
+    let y=2;
+    console.log(x,y)
+}
+otraFuncion();
